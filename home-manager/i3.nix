92270d9446196ae6e44b5a518b14319e03de1149
Paul Dufour <paul.dufour@epita.fr>
2024-07-15 22:04:35 +0200

    feat: i3 config border none
    Signed-off-by: Paul Dufour <paul.dufour@epita.fr>

--- home-manager/i3.nix
+++ home-manager/i3.nix
@@ -6,10 +6,19 @@
     config = {
       modifier = "Mod4";
       terminal = "alacritty";
+      window = {
+        hideEdgeBorders = "none";
+      };
       bars = [{
         position = "top";
         statusCommand = "${pkgs.i3status}/bin/i3status";
       }];
     };
+    extraConfig = ''
+      #Hide window title bar
+      default_border pixel 1
+      default_floating_border pixel 1
+      for_window [class="^.*"] border pixel 1
+      '';
   };
 }
