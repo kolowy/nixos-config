e1cb2d0e49b9d378dc2046d2a8e4b7f5384873c1
Paul Dufour <paul.dufour@epita.fr>
2024-12-19 10:40:58 +0100

    feat: update pulseaudio
    Signed-off-by: Paul Dufour <paul.dufour@epita.fr>

--- modules/nixos/sound.nix
+++ modules/nixos/sound.nix
@@ -5,9 +5,9 @@
   modulesPath,
   ...
 }: {
-  #hardware.pulseaudio.package = pkgs.pulseaudioFull;
-  #hardware.pulseaudio.enable = true;
-  #hardware.pulseaudio.enable = true;
-  nixpkgs.config.pulseaudio = false;
-  #hardware.pulseaudio.extraConfig = "load-module module-combine-sink";
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
+  hardware.pulseaudio.enable = true;
+  nixpkgs.config.pulseaudio = true;
+  hardware.pulseaudio.extraConfig = "load-module module-combine-sink";
+  services.pipewire.enable = false;
 }
